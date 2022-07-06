3aa9d9c0d2d477cce39a6aa2625331eaee4c4361
JanukanS <sivajeya@ualberta.ca>
2022-07-05 22:46:18 -0600

    Added test cases for Address.express()

--- address_test.go
+++ address_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func testExpress(t *testing.T) {
+func TestExpress(t *testing.T) {
 	addr1 := Address{username: "someone",
 		ip:   "144.144.144.144",
 		port: 22,
@@ -14,4 +14,12 @@ func testExpress(t *testing.T) {
 
 	expr1 := addr1.express()
 	assert.Equal(t, expr1, "someone@144.144.144.144", "should equal")
+
+	addr1.port = 23
+	expr2 := addr1.express()
+	assert.Equal(t, expr2, "someone@144.144.144.144:23", "Should be Equal")
+
+	addr1.username = ""
+	expr3 := addr1.express()
+	assert.Equal(t, expr3, "144.144.144.144:23", "Should be equal")
 }
